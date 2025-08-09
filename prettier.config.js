6f78e7a60a09fc3e5d7ffc5dd4d5f2df21461af3
Sanni Abdulmuiz <ayomideabdulmuiz565@gmail.com>
2025-08-09 13:15:51 +0100

    Initial Commit

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+import 'prettier-plugin-tailwindcss';
+export default {
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+// {
+//   "plugins": ["prettier-plugin-tailwindcss"]
+// }
+// module.exports = {
+//   plugins: [require("prettier-plugin-tailwindcss")],
+//   singleQuote: true,
+// };
